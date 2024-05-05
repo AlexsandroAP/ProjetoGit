fe62c920ca20f0eceeaa1c4f153fc5b59fb24277
Alexsandro Lima Nogueira <alexpotteralma@gmail.com>
2024-05-05 14:56:33 -0300

    Read Feito

--- resources/views/listarProduto.blade.php
+++ resources/views/listarProduto.blade.php
@@ -14,11 +14,13 @@
                 <button class="rounded-lg bg-green-500 text-white p-4 float-left mb-9">Criar Produto</button>
             </a>
 
-                <ul>
+                <!-- <ul>
                     @foreach ($produtos as $produto)
                         <li>{{ $produto->nome}}</li>
+                        <li>{{ $produto->quantidade}}</li>
+                        <li>{{ $produto->preco}}</li>
                     @endforeach
-                </ul>
+                </ul> -->
 
 
 
@@ -36,34 +38,36 @@
                         </tr>
                     </thead>
 
+                    @foreach ($produtos as $produto)
                     <tbody>
                         <tr class="bg-gray-100 border-b">
-                            <td class="px-6 py-4 whitespace-nowrap text-black text-center">Feijão</td>
-
-                            <td class="px-6 py-4 text-black whitespace-nowrap text-center">12</td>
-
-                            <td class="px-6 py-4 text-black whitespace-nowrap text-center">55,3</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-black text-center">
+                                
+                                    <li>{{ $produto->nome}}</li>
+                                
+                            </td>
 
                             <td class="px-6 py-4 text-black whitespace-nowrap text-center">
-                                <button class="rounded-lg bg-red-500 text-white p-4">Excluir</button>
-                                <button class="rounded-lg bg-yellow-500 text-white p-4">Editar</button>
+                                    <li>{{ $produto->quantidade}}</li>
+                              
+                            </td>
+
+                            <td class="px-6 py-4 text-black whitespace-nowrap text-center">
+                                    <li>{{ $produto->precoTotal}}</li>
+                                
+                            </td>
+                            
+                            <td class="px-6 py-4 text-black whitespace-nowrap text-center">
+                               
+                                    <button class="rounded-lg bg-red-500 text-white p-4">Excluir</button>
+                                
+                                <button class="rounded-lg bg-blue-500 text-white p-4">Editar</button>
                             </td>
                         </tr>
 
-                        <tr class="bg-white border-b">
-                            <td class="px-6 py-4 text-black whitespace-nowrap text-center">Arroz</td>
-
-                            <td class="px-6 py-4 whitespace-nowrap text-black text-center">100</td>
-
-                            <td class="px-6 py-4 text-black whitespace-nowrap text-center">100,50</td>
-
-                            <td class="px-6 py-4 text-black whitespace-nowrap text-center">
-                                <button class="rounded-lg bg-red-500 text-white p-4">Excluir</button>
-                                <button class="rounded-lg bg-yellow-500 text-white p-4">Editar</button>
-                            </td>
-                        </tr>
-        
                     </tbody>
+
+                    @endforeach
                     </table>
 
                 </div>
