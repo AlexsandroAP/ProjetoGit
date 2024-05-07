7403f1cbb1c35841808a66348d5bf46caad0aae3
Alexsandro Lima Nogueira <alexpotteralma@gmail.com>
2024-05-07 06:25:34 -0300

    controleerProduto adicionado o edit

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -46,9 +46,9 @@ class ProdutoController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Produto $produto)
     {
-        //
+        return view('editarProduto', ['produto' => $produto]);
     }
 
     /**
