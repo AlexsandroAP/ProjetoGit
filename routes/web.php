16321915348a83ee2a43a819ee14bdaa0819838d
Alexsandro Lima Nogueira <alexpotteralma@gmail.com>
2024-05-07 06:27:17 -0300

    Retirado o plural dos parâmetros do web

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/produtos', [ProdutoController::class, 'index'])->name('produtos.index');
     Route::get('/produtos/create', [ProdutoController::class, 'create'])->name('produtos.create');
     Route::post('/produtos', [ProdutoController::class, 'store'])->name('produtos.store');
-    Route::get('/produtos/{produtos}', [ProdutoController::class, 'show'])->name('produtos.show');
-    Route::get('/produtos/{produtos}/edit', [ProdutoController::class, 'edit'])->name('produtos.edit');
-    Route::put('/produtos/{produtos}', [ProdutoController::class, 'update'])->name('produtos.update');
-    Route::delete('/produtos/{produtos}', [ProdutoController::class, 'destroy'])->name('produtos.destroy');
+    Route::get('/produtos/{produto}', [ProdutoController::class, 'show'])->name('produtos.show');
+    Route::get('/produtos/{produto}/edit', [ProdutoController::class, 'edit'])->name('produtos.edit');
+    Route::put('/produtos/{produto}', [ProdutoController::class, 'update'])->name('produtos.update');
+    Route::delete('/produtos/{produto}', [ProdutoController::class, 'destroy'])->name('produtos.destroy');
 
 });
 
