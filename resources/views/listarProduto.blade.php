f5d189cdbe6db798008dc9c06b0cdd1e47d834e5
Alexsandro Lima Nogueira <alexpotteralma@gmail.com>
2024-05-10 19:11:58 -0300

    botão para retirar alert adicionado

--- resources/views/listarProduto.blade.php
+++ resources/views/listarProduto.blade.php
@@ -6,10 +6,14 @@
     </x-slot>
 
     @if (session()->has('mensagem'))
-        <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
-            {{ session()->get('mensagem') }}
-        </div>
+    <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 relative" role="alert" id="meuAlerta">
+        <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
+            <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
+        </span>
+        {{ session()->get('mensagem') }}
+    </div>
     @endif
+
 
     <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
@@ -55,7 +59,9 @@
                             
                             <td class="px-6 py-4 text-black whitespace-nowrap text-center">
                                
-                                <button class="rounded-lg bg-red-500 text-white p-4">Excluir</button>
+                            <a href="{{ route('produtos.show',['produto' => $produto->id]) }}">
+                                    <button class="rounded-lg bg-red-500 text-white p-4">Excluir</button>
+                                </a>
                                 
                                 <a href="{{ route('produtos.edit',['produto' => $produto->id]) }}">
                                     <button class="rounded-lg bg-blue-500 text-white p-4">Editar</button>
