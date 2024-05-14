ed0482d86e84d4fc10a0dcf7920a04104e7dde32
AlexsandroAP <alexpotteralma@gmail.com>
2024-05-14 16:51:42 -0300

    mudança do form

--- resources/views/editarProduto.blade.php
+++ resources/views/editarProduto.blade.php
@@ -9,30 +9,30 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <h1 class="text-3xl mb-10">Cadastro de produtos</h1>
+                        <h1 class="text-3xl mb-10">Editar o produto:
+                            <br>
+                             {{ $produto->nome }}</h1>
 
                         <form method="post" action="{{ route('produtos.update',['produto' => $produto->id]) }}">
                         @csrf
                             <input type="hidden" name="_method" value="PUT">
 
-                            <h1>Edite o produto:</h1>
-                            <input class="text-black" type="text" name="nome" value="{{ $produto->nome }}">
-                            
-                           
+                            <h1 class="mt-10">Edite o produto:</h1>
+                            <input class=" w-64 h-10 mt-2 text-black rounded-lg" type="text" name="nome" value="{{ $produto->nome }}">
 
-                            <h1 class="mt-7">Quantidade dos produtos:</h1>
-                            <input class="text-black" type="number" name="quantidade" value="{{ $produto->quantidade }}">
+                            <h1 class="mt-10">Quantidade dos produtos:</h1>
+                            <input class=" w-64 h-10 mt-2 text-black rounded-lg" type="number" name="quantidade" value="{{ $produto->quantidade }}">
 
-                            <h1 class="mt-7">Preço total dos produtos:</h1>
-                            <input class="text-black" type="number" 
+                            <h1 class="mt-10">Preço total dos produtos:</h1>
+                            <input class=" w-64 h-10 mt-2 text-black rounded-lg" type="number" 
                             step="0.01" name="preco" value="{{ $produto->preco }}">
 
-                            <div class="mt-4 block mt-7">
+                            <div class="mt-10 block mb-5">
                                 <a href="{{ route('produtos.index') }}">
                                     <button type="button" class="rounded-lg bg-vermelho text-white p-4">Cancelar</button>
                                 </a>
 
-                                <button type="submit" class="rounded-lg bg-verde-claro text-white p-4 px-6 text-medium">Salvar</button>
+                                <button type="submit" class="ml-2 px-6 rounded-lg bg-verde-claro text-white p-4">Salvar</button>
                             </div>
                         </form>
                     
