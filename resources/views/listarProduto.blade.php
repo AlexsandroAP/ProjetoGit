ae435099ffdaf9d8278fc34abc1b19b3bd068019
AlexsandroAP <alexpotteralma@gmail.com>
2024-05-14 15:52:15 -0300

    mensagem no centro

--- resources/views/listarProduto.blade.php
+++ resources/views/listarProduto.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     @if (session()->has('mensagemCriar'))
-    <div class="bg-green-100 border-l-4 border-green-500 text-black-700 p-4 relative" role="alert" id="meuAlerta">
+    <div class="bg-green-100 border-1-4 border-green-500 text-black-700 p-4 relative" role="alert" id="meuAlerta">
         <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
             <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
         </span>
@@ -15,10 +15,12 @@
     @endif
 
     @if (session()->has('mensagemEditar'))
-    <div class="bg-yellow-100 border-l-4 border-yellow-500 text-black-700 p-4 relative" role="alert" id="meuAlerta">
+    <div class="bg-yellow-100 border-l-4 border-yellow-500 text-black-700 p-3 max-w-md mx-auto block relative mt-5" role="alert" id="meuAlerta">
+
         <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
             <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
         </span>
+        
         {{ session()->get('mensagemEditar') }}
     </div>
     @endif
@@ -38,21 +40,23 @@
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
 
-            <a href="{{route('produtos.create')}}">
-                <button class="rounded-lg bg-green-500 text-white p-4 float-left mb-9">Criar Produto</button>
-            </a>
+            
 
                 
                     <table class="min-w-full border border-gray-300">
                     <thead>
                         <tr>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b text-center">Nome do produto</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider border-b text-center">Nome do produto</th>
 
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b text-center">Quantidade</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider border-b text-center">Quantidade</th>
 
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b text-center">Preço</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider border-b text-center">Preço</th>
 
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b text-center"></th>
+                            <th class="px-6 py-3 text-left font-thin uppercase text-center">
+                            <a href="{{route('produtos.create')}}">
+                                <button class="rounded-lg bg-verde-claro text-white p-3.5 px-8">Criar Produto</button>
+                            </a>
+                            </th>
                         </tr>
                     </thead>
 
@@ -61,28 +65,28 @@
                         <tr class="bg-gray-100 border-b">
                             <td class="px-6 py-4 whitespace-nowrap text-black text-center">
                                 
-                                    <li>{{ $produto->nome}}</li>
+                                    <p>{{ $produto->nome}}</p>
                                 
                             </td>
 
                             <td class="px-6 py-4 text-black whitespace-nowrap text-center">
-                                    <li>{{ $produto->quantidade}}</li>
+                                    <p>{{ $produto->quantidade}}</p>
                               
                             </td>
 
                             <td class="px-6 py-4 text-black whitespace-nowrap text-center">
-                                    <li>{{ $produto->preco}}</li>
+                                    <p>{{ $produto->preco}}</p>
                                 
                             </td>
                             
                             <td class="px-6 py-4 text-black whitespace-nowrap text-center">
                                
                             <a href="{{ route('produtos.show',['produto' => $produto->id]) }}">
-                                    <button class="rounded-lg bg-red-500 text-white p-4">Excluir</button>
+                                    <button class="rounded-lg bg-vermelho text-white p-4">Excluir</button>
                                 </a>
                                 
                                 <a href="{{ route('produtos.edit',['produto' => $produto->id]) }}">
-                                    <button class="rounded-lg bg-blue-500 text-white p-4">Editar</button>
+                                    <button class="rounded-lg bg-yellow-500 text-white p-4">Editar</button>
                                 </a>
                             </td>
                         </tr>
