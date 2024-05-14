ab2edafb6faa9bd9e1fbe40ca32af5e7568caee9
AlexsandroAP <alexpotteralma@gmail.com>
2024-05-14 16:51:51 -0300

    mudança do form

--- resources/views/excluirProduto.blade.php
+++ resources/views/excluirProduto.blade.php
@@ -9,7 +9,9 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <h1 class="text-3xl mb-10">Excluir produto: {{ $produto->nome }}</h1>
+                        <h1 class="text-3xl mb-10">Excluir produto: 
+                            <br>
+                            {{ $produto->nome }}</h1>
 
                         <form method="post" action="{{route('produtos.destroy', ['produto' => $produto->id])}}">
                         
@@ -17,25 +19,23 @@
                             <input type="hidden" name="_method" value="DELETE">
 
                             <h1>Produto:</h1>
-                            <input class="text-black" type="text" name="nome" readonly value="{{ $produto->nome }}">
+                            <input class=" w-64 h-10 mt-2 text-black rounded-lg" type="text" name="nome" readonly value="{{ $produto->nome }}">
                             
-                           
-
                             <h1 class="mt-7">Quantidade dos produtos:</h1>
-                            <input class="text-black" type="number" name="quantidade" readonly value="{{ $produto->quantidade }}">
+                            <input class=" w-64 h-10 mt-2 text-black rounded-lg" type="number" name="quantidade" readonly value="{{ $produto->quantidade }}">
 
                             <h1 class="mt-7">Preço total dos produtos:</h1>
-                            <input class="text-black" type="number" 
+                            <input class=" w-64 h-10 mt-2 text-black rounded-lg" type="number" 
                             step="0.01" 
                             readonly name="preco" value="{{ $produto->preco }}">
 
 
-                            <div class="mt-4 block mt-7">
+                            <div class="mt-10 block mb-5">
                                 <a href="{{ route('produtos.index') }}">
-                                    <button type="button" class="rounded-lg bg-yellow-500 text-white p-4 px-4">Cancelar</button>
+                                    <button type="button" class="rounded-lg bg-yellow-500 text-white p-4">Cancelar</button>
                                 </a>
 
-                                <button type="submit" class="rounded-lg bg-vermelho text-white p-4">Deletar</button>
+                                <button type="submit" class="ml-2 rounded-lg bg-vermelho text-white p-4">Deletar</button>
                             </div>
                         </form>
                     
