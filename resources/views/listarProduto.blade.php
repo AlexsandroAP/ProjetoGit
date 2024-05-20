617af897e0a6e911c33ad2b057c3fa2bd592ff7e
AlexndroAP <alexpotteralma@gmail.com>
2024-05-20 11:51:23 -0300

    correção do alert

--- resources/views/listarProduto.blade.php
+++ resources/views/listarProduto.blade.php
@@ -32,13 +32,8 @@
         
             @if (session()->has('mensagemDeletar'))
             <div class="bg-red-100 border-l-4 border-red-500 text-black p-3 max-w-md mx-auto block relative mb-5" role="alert" id="meuAlerta">
-
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.   style.display='none';">
-                <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
-
                 <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
                 <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
-
                 </span>
                 {{ session()->get('mensagemDeletar') }}
             </div>
