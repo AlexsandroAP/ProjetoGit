92dde025a7fae6abfbf9669050bfd013b5174988
AlexsandroAP <alexpotteralma@gmail.com>
2024-05-21 15:59:44 -0300

    resgistro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,21 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
     
     <div class="container mx-auto p-4">
-        <h1 class="text-3xl font-bold mb-4 text-white">Bem Vindo {{ Auth::user()->name }}!</h1>
-        <p class="text-white">Seja bem-vindo ao nosso site. Esperamos que você aproveite sua visita.</p>
+        <h1 class="text-3xl font-bold mb-4 text-white">Bem Vindo</h1>
+        <p class="text-white">Seja bem-vindo ao nosso site. Esperamos que você aproveite sua visita. Começe...</p>
         
 
-        <a href="{{route('produtos.index')}}">
-            <x-primary-button class="mt-4">
-                {{ __('Entrar') }}
+        <a href="{{route('register')}}">
+            <x-primary-button class="mt-4 ">
+                {{ __('Cadastre-se') }}
             </x-primary-button>
         </a>
         
+        <a href="{{route('produtos.index')}}">
+            <x-primary-button class="mt-4 ml-2">
+                {{ __('Login') }}
+            </x-primary-button>
+        </a>
     
     </div>
     
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/', function () {return view('welcome');});
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
