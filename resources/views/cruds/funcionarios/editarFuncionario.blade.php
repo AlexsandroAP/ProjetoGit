c21b9a6bab832a7fa8b21b5857a38aee06a04e26
AlexsandroAP <alexpotteralma@gmail.com>
2024-06-17 11:36:03 -0300

    menagens

--- resources/views/cruds/funcionarios/editarFuncionario.blade.php
+++ resources/views/cruds/funcionarios/editarFuncionario.blade.php
@@ -5,17 +5,6 @@
         </h2>
     </x-slot>
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <div class="mt-5 bg-red-100 border-l-4 border-red-500 text-black p-3 mx-auto block relative max-w-md" role="alert" id="meuAlerta">
-                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
-                    <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
-                    </span>
-                    {{ $error }}
-                </div>
-        @endforeach
-    @endif
-
     <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -29,11 +18,44 @@
                             <h1 class="mt-10">Edite o(a) nome funcionario(a):</h1>
                             <input class=" w-64 h-10 mt-2 text-black rounded-lg" type="text" name="nome" value="{{ $funcionario->nome }}">
 
+                            @if ($errors->any())
+        @foreach ($errors->get('nome') as $error)
+            <div class="text-red-500" role="alert" id="meuAlerta">
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
+                    <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
+                    </span>
+                    {{ $error }}
+                </div>
+        @endforeach
+    @endif
+
                             <h1 class="mt-10">Edite o(a) cargo do(a) funcionario(a):</h1>
                             <input class=" w-64 h-10 mt-2 text-black rounded-lg" type="text" name="cargo" value="{{ $funcionario->cargo }}">
 
+                            @if ($errors->any())
+        @foreach ($errors->get('cargo') as $error)
+            <div class="text-red-500" role="alert" id="meuAlerta">
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
+                    <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
+                    </span>
+                    {{ $error }}
+                </div>
+        @endforeach
+    @endif
+
                             <h1 class="mt-10">Edite o(a) CPF do(a)funcionario(a):</h1>
                             <input class=" w-64 h-10 mt-2 text-black rounded-lg" type="text" name="cpf" value="{{ $funcionario->cpf }}">
+
+                            @if ($errors->any())
+        @foreach ($errors->get('cpf') as $error)
+            <div class="text-red-500" role="alert" id="meuAlerta">
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
+                    <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
+                    </span>
+                    {{ $error }}
+                </div>
+        @endforeach
+    @endif
 
                             <div class="mt-10 block mb-5">
                                 <a href="{{ route('funcionarios.index') }}">
--- resources/views/cruds/produtos/editarProduto.blade.php
+++ resources/views/cruds/produtos/editarProduto.blade.php
@@ -22,17 +22,6 @@
         </h2>
     </x-slot>
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <div class="mt-5 bg-red-100 border-l-4 border-red-500 text-black p-3 mx-auto block relative max-w-md" role="alert" id="meuAlerta">
-                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
-                    <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
-                    </span>
-                    {{ $error }}
-                </div>
-        @endforeach
-    @endif
-
     <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -46,28 +35,71 @@
                             <h1 class="mt-10">Edite o produto:</h1>
                             <input class=" w-64 h-10 mt-2 text-black rounded-lg" type="text" name="nome" value="{{ $produto->nome }}">
 
+                            @if ($errors->any())
+        @foreach ($errors->get('nome') as $error)
+            <div class="text-red-500" role="alert" id="meuAlerta">
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
+                    <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
+                    </span>
+                    {{ $error }}
+                </div>
+        @endforeach
+    @endif
+
                             <div>
                                 <h1 class="mt-10">Selecione a categoria:</h1>
-                                <select name="categoria" class="w-64 mt-2 text-gray-600 rounded-lg">
-                                    <option>Nenhuma</option>
+                                <select name="categoria" class="w-64 mt-2 text-black rounded-lg">
                                     <?php
                                     if ($result->rowCount() > 0) {
                                         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
                                             echo '<option>' . $row["nome"] . '</option>';
                                         }
                                     } else {
-                                        echo '<option value="">Insira uma categoria primeiro!</option>';
+                                        echo '<option value="">Nenhuma</option>';
                                     }
                                     ?>
                                 </select>
                             </div>
+
+                            @if ($errors->any())
+        @foreach ($errors->get('categoria') as $error)
+            <div class="text-red-500" role="alert" id="meuAlerta">
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
+                    <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
+                    </span>
+                    {{ $error }}
+                </div>
+        @endforeach
+    @endif
                             
                             <h1 class="mt-10">Quantidade dos produtos:</h1>
                             <input min="0" max="100000" class=" w-64 h-10 mt-2 text-black rounded-lg" type="number" name="quantidade" value="{{ $produto->quantidade }}">
 
+                            @if ($errors->any())
+        @foreach ($errors->get('quantidade') as $error)
+            <div class="text-red-500" role="alert" id="meuAlerta">
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
+                    <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
+                    </span>
+                    {{ $error }}
+                </div>
+        @endforeach
+    @endif
+
                             <h1 class="mt-10">Preço total dos produtos:</h1>
                             <input min="0" max="100000" class=" w-64 h-10 mt-2 text-black rounded-lg" type="number"
                             step="0.01" name="preco" value="{{ $produto->preco }}">
+
+                            @if ($errors->any())
+        @foreach ($errors->get('preco') as $error)
+            <div class="text-red-500" role="alert" id="meuAlerta">
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 closebtn" onclick="this.parentElement.style.display='none';">
+                    <svg class="fill-current h-6 w-6 text-black" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path></svg>
+                    </span>
+                    {{ $error }}
+                </div>
+        @endforeach
+    @endif
 
                             <div class="mt-10 block mb-5">
                                 <a href="{{ route('produtos.index') }}">
